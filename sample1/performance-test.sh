38b4483766640a67a9cbe8278dac04fca2a1743d
Davin Fausta <davinfausta@outlook.co.id>
2026-06-24 08:41:56 +0700

    Add performance testing to sample1, fix pathing and minikube IP detection

--- sample1/performance-test.sh
+++ sample1/performance-test.sh
@@ -4,12 +4,12 @@ set -x
 N=100
 
 NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="InternalIP")].address }')
-NODE_PORT=$(kubectl get svc hello -o=jsonpath='{.spec.ports[0].nodePort}')
+NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
 ENDPOINT="${NODE_IP}:${NODE_PORT}"
 
 START=$(date +%s)
 for i in $(seq ${N}); do
-	curl http://${ENDPOINT}/hello
+	curl "http://${ENDPOINT}/sum?a=1&b=2"
 done
 END=$(date +%s)
 
