289eca40ab78049dcfde595adb802e6966fa7ece
Davin Fausta <davinfausta@outlook.co.id>
2026-06-24 19:27:23 +0700

    Update acceptance-test.sh

--- sample1/acceptance-test.sh
+++ sample1/acceptance-test.sh
@@ -2,5 +2,11 @@
 set -x
 
 NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="ExternalIP")].address }')
+
+# Fallback ke InternalIP jika ExternalIP tidak tersedia (minikube)
+if [ -z "$NODE_IP" ]; then
+  NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="InternalIP")].address }')
+fi
+
 NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
 ./gradlew acceptanceTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
