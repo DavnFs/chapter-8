9455b39122f032f609a76f875dd834b712f69c67
Davin Fausta <davinfausta@outlook.co.id>
2026-06-25 13:49:19 +0700

    Update acceptance-test.sh

--- sample1/acceptance-test.sh
+++ sample1/acceptance-test.sh
@@ -3,4 +3,12 @@ set -x
 
 NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="InternalIP")].address }')
 NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
+
+echo "Menunggu Spring Boot siap di http://${NODE_IP}:${NODE_PORT}..."
+until curl -s --connect-timeout 2 http://${NODE_IP}:${NODE_PORT} > /dev/null; do
+     echo "Aplikasi masih loading context internal, menunggu 3 detik lagi..."
+     sleep 3
+done
+
+echo "Aplikasi aktif! Memulai Acceptance Test..."
 ./gradlew acceptanceTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
